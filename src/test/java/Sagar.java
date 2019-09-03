ff3f56e43695ee774a0347e74c123410dfb6efce
sagar21190 <sagarmahangare90@gmail.com>
2019-09-03 15:32:39 +0530

    add src/test

---
+++ src/test/java/Sagar.java
@@ -0,0 +1,17 @@
+package com.mobileware.npci.engine;
+
+public class Sagar {
+
+	public static void main(String[] args) {
+		// String
+		// url="http://10.0.10.2:1010/BillPaymentRequest/1.0/urn:referenceId:D03EQSXAH7LBPT3Q16NWIVRKUOO7TDS5QBH";
+		// String[] arr= url.split(":");
+		// System.out.println(arr[2].split("//"));
+		// CustBalDetailsResp resp =new CustBalDetailsResp();
+		// resp.setConsumerId("122111");
+		// String gson=new Gson().toJson(resp);
+		System.out.println("Hello User!");
+
+	}
+
+}
